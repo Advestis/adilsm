6a3257686d9ff736b5a8406666c51e1a275ebbd6
Victor Chemla <victor.chemla@mazars.fr>
2024-03-11 18:38:06 +0100

    [add] desc for pkg

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import find_packages, setup
 import versioneer
 requirements = """
-setuptools>=61
 adnmtf==0.1.164
 """
 
+description = "Integrated Longitudinal Multi Source Model"
+
 setup(
     name="adilsm",
-    version='0.0.1',
+    version='0.0.2',
     author="Mazars",
     author_email="victor.chemla@mazars.fr",
-    description="{description}",
+    description=f"{description}",
     packages=find_packages(),
     install_requires=requirements,
     # include_package_data: to install data from MANIFEST.in
