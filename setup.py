5cd81050527221e922bd728bdf6e4aa8d3e173d2
Victor Chemla <victor.chemla@mazars.fr>
2024-03-18 15:12:58 +0100

    [add] some corection

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ description = "Integrated Longitudinal Multi Source Model"
 
 setup(
     name="adilsm",
-    version='0.0.6',
+    version='0.0.7',
     author="Mazars",
     author_email="victor.chemla@mazars.fr",
     description=f"{description}",
